995d0fd8dea31103b33bff32d1a6d2e7aaa4fda8
GraceRuan <grace.ruan@gov.bc.ca>
2025-06-05 16:20:32 -0700

    feat: add more informationon web page

---
+++ src/config/configuration.ts
@@ -0,0 +1,3 @@
+export default () => ({
+  apitoken: process.env.APITOKEN || '',
+});
---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const PROJECT_NAME = "nodejs-sample"
+export const SERVICE_NAME = "nodejs-sample"
+export const BROKER_URL = "https://broker.io.nrs.gov.bc.ca"
